6f7fc1b3daa05c46655119aef8f5ef04ca164511
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-04 01:32:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,9 @@ def monopoly_with_third_tier
     :railroads => 
     {
       :pieces => 4,
-      :rent_in_dollars => 4
+      :rent_in_dollars => {
+        :key1 => "value",
+      }
     }
   }
   
